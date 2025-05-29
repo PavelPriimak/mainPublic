aa3bb6fd1d3a6d1a9ba78ccd53333189f20f94bb
PavelPriimak <priymakpavel@mail.ru>
2025-05-29 20:00:32 +0300

    fixed link

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,12 +3,11 @@ import Board from '../board/board';
 import TaskDetail from '../task-detail/TaskDetail';
 import { Routes, Route } from 'react-router-dom';
 
-const Main = (props) => {
+const Main = (props) => { 
   return (
-    // eslint-disable-next-line no-undef
     <main className={css.main}>
       <Routes>
-        <Route path={'/'} element={<Board {...props} />} />
+        <Route exact path={'/'} element={<Board {...props} />} />
         <Route path={'/tasks/:taskId'} element={<TaskDetail {...props} />} />
       </Routes>
     </main>

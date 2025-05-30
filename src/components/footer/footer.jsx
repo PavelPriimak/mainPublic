b22e5ae9fcbbcc5ff8fdb5dc72eb8ddbe619a976
PavelPriimak <priymakpavel@mail.ru>
2025-05-30 21:32:58 +0300

    new9

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,12 +2,12 @@ import css from './footer.module.css'
 
 
 
-function Footer() {
-	
+function Footer(props) {
+	const {activeTasks, finishedTasks} = props
 	return (
 		<footer className={css.footer}>
-			<div className={css.active_tasks}>Active tasks:</div>
-			<div className={css.finished_tasks}>Finished tasks:</div>
+			<div className={css.active_tasks}>Active tasks:{activeTasks}</div>
+			<div className={css.finished_tasks}>Finished tasks:{finishedTasks}</div>
 			<div className={css.author}>Kanban board by Pavel Priymak</div>
 		</footer>
 	)
